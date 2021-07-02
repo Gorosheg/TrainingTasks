cbf1a22fbccc7936bd4210465f171b0fe28d8726
EduardM <eduard.mishustin@yandex.ru>
2021-07-03 00:12:02 +0300

    Draw figure fixes and add review comments

--- app/src/main/java/com/example/lessonsFromSamsung/theme27_2Dgraphics/ticTacToe/TicTacToeDraw.kt
+++ app/src/main/java/com/example/lessonsFromSamsung/theme27_2Dgraphics/ticTacToe/TicTacToeDraw.kt
@@ -7,32 +7,36 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.view.MotionEvent
 import android.view.View
+import androidx.annotation.ColorInt
 import com.example.lessonsFromSamsung.utils.createArrayWithZero
 
 class TicTacToeDraw(context: Context) : View(context) {
 
     private val array = createArrayWithZero(9)
-    private var pointX = 0f
-    private var pointY = 0f
+    private var pointX = 0f // TODO: делаем локальными и передаём в функцию параметром
+    private var pointY = 0f // TODO: делаем локальными и передаём в функцию параметром
     private var queue = true
 
-    private val linePaint: Paint = Paint().apply {
+    private val linePaint: Paint = Paint().apply { // TODO: Use buildPaint
         style = Paint.Style.STROKE
         color = Color.GRAY
         strokeWidth = 7f
     }
 
-    private val crossLinePaint: Paint = Paint().apply {
-        style = Paint.Style.STROKE
-        color = Color.BLUE
-        strokeWidth = 7f
-    }
+    private val crossLinePaint: Paint = buildPaint(Color.BLUE)
 
-    private val circlePaint: Paint = Paint().apply {
-        isAntiAlias
+    private val circlePaint: Paint = Paint().apply { // TODO: Use buildPaint
         style = Paint.Style.STROKE
         color = Color.RED
         strokeWidth = 7f
+    }
+
+    private fun buildPaint(@ColorInt color: Int): Paint {
+        return Paint().apply {
+            style = Paint.Style.STROKE
+            this.color = color
+            strokeWidth = 7f
+        }
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -43,7 +47,6 @@ class TicTacToeDraw(context: Context) : View(context) {
         drawFigures(canvas)
     }
 
-
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent): Boolean {
         when (event.actionMasked) {
@@ -53,6 +56,7 @@ class TicTacToeDraw(context: Context) : View(context) {
                 invalidate()
             }
         }
+
         return true
     }
 
@@ -60,8 +64,7 @@ class TicTacToeDraw(context: Context) : View(context) {
         val x = (pair.first / C).toInt()
         val y = ((pair.second - C) / C).toInt()
         val index = 3 * y + x
-        if (queue) array[index] = 1
-        else array[index] = 2
+        array[index] = if (queue) 1 else 2
         queue = !queue
     }
 
@@ -73,7 +76,7 @@ class TicTacToeDraw(context: Context) : View(context) {
         }
     }
 
-    private fun writeCoordinates(i: Int, canvas: Canvas){
+    private fun writeCoordinates(i: Int, canvas: Canvas) { // TODO: drawFigure
         val line = i / 3
         val column = i % 3
 
@@ -83,31 +86,28 @@ class TicTacToeDraw(context: Context) : View(context) {
             2 -> D
             else -> A
         }
+
         pointY = when (line) {
             0 -> C
             1 -> D
             2 -> E
             else -> A
         }
-        drawCross(canvas)
-        drawCircle(canvas)
+
+        if (array[i] == 1) {
+            drawCross(canvas)
+        } else if (array[i] == 2) {
+            drawCircle(canvas)
+        }
     }
 
     private fun drawCross(canvas: Canvas) {
-        for (i in array.indices) {
-            if (array[i] == 1) {
-                drawLine(pointX, pointY, pointX + C, pointY + C, canvas, crossLinePaint)
-                drawLine(pointX + C, pointY, pointX, pointY + C, canvas, crossLinePaint)
-            }
-        }
+        drawLine(pointX, pointY, pointX + C, pointY + C, canvas, crossLinePaint)
+        drawLine(pointX + C, pointY, pointX, pointY + C, canvas, crossLinePaint)
     }
 
     private fun drawCircle(canvas: Canvas) {
-        for (i in array.indices) {
-            if (array[i] == 2) {
-                canvas.drawCircle(pointX + B, pointY + B, B, circlePaint)
-            }
-        }
+        canvas.drawCircle(pointX + B, pointY + B, B, circlePaint)
     }
 
     private fun drawGrid(canvas: Canvas) {
@@ -122,12 +122,14 @@ class TicTacToeDraw(context: Context) : View(context) {
     }
 
     companion object {
+
         private const val A = 0f
         private const val B = 175f
         private const val C = 350f
         private const val D = 700f
         private const val E = 1050f
         private const val F = 1400f
+
     }
 
 }
