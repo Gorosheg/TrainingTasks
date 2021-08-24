b22462c7caa9dfc1b5613fcaf24db4fe1b62c504
AnnaG <anutagoroxowa@yandex.ru>
2021-08-24 12:54:04 +0300

    Add: theme 30.5 Recycler view

---
+++ app/src/main/java/com/example/lessonsFromSamsung/theme30_5_RecyclerView/RecyclerViewActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lessonsFromSamsung.theme30_5_RecyclerView
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.lessonsFromSamsung.R
+
+class RecyclerViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_theme_30_5_recycler_view)
+    }
+}
