0cbb1f10f464aad8c608f1cd3cb32f88c21767b7
AnnaG <anutagoroxowa@yandex.ru>
2021-08-24 12:40:38 +0300

    Add(ClientServerArchitecture): add comments to EnumExampleUsage

--- app/src/main/java/com/example/lessonsFromSamsung/theme49_ClientServerArchitecture/EnumExampleUsage.kt
+++ app/src/main/java/com/example/lessonsFromSamsung/theme49_ClientServerArchitecture/EnumExampleUsage.kt
@@ -1,22 +1,26 @@
 package com.example.lessonsFromSamsung.theme49_ClientServerArchitecture
 
+/**
+ * enum - ограниченное перечисление.
+ * При вызове Enum не вызыается конструктор.
+ * Enum - константа. При его вызове нельзя передавать свои параметры.
+ */
 enum class ExampleEnum {
-
     CLEAR,
     SNOW;
-
 }
 
-private const val CONST1 = 1
-private const val CONST2 = 2
+private var field1 = 1
+private var field2 = 2
 
-private val Int.exampleField: String
+private val Int.exampleField: Unit
     get() = when (this) {
-        1 -> TODO()
-        2 -> TODO()
-        else -> TODO()
+        1 -> field1 = 2
+        2 -> field2 = 1
+        else -> field1 = 3
     }
 
+// Если перебирать enum в when, то не нужна ветка else.
 private val ExampleEnum.exampleField: String
     get() = when (this) {
         ExampleEnum.CLEAR -> TODO()
