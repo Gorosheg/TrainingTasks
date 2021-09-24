ae321921bf98d2e9a45a5b97e7ba33c72c127291
AnnaG <anutagoroxowa@yandex.ru>
2021-09-24 11:14:36 +0300

    Add: Activity for adding products

---
+++ app/src/main/java/com/example/lessonsFromSamsung/theme45_SQLite/AddElementsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lessonsFromSamsung.theme45_SQLite
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.lessonsFromSamsung.R
+
+class AddElementsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_theme_45_add_elements)
+    }
+}
