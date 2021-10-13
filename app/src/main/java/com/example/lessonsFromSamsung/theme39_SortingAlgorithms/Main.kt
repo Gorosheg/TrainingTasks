64792589ff7b66622b8490930b21c0bedfdfae54
AnnaG <anutagoroxowa@yandex.ru>
2021-10-13 17:50:18 +0300

    Complete: theme 39 Sorting algorithms

--- app/src/main/java/com/example/lessonsFromSamsung/theme39_SortingAlgorithms/Main.kt
+++ app/src/main/java/com/example/lessonsFromSamsung/theme39_SortingAlgorithms/Main.kt
@@ -1,17 +1,48 @@
 package com.example.lessonsFromSamsung.theme39_SortingAlgorithms
 
-class Main {
+fun main() {
+    val list = studentsList()
+    val sortingArray = sortStudents(list)
+    printPersons(sortingArray)
+}
 
-    private fun studentsList(): ArrayList<Person> = arrayListOf(
-        Person("Anna", "Ivanova", 12),
-        Person("Ivan", "Petrov", 14),
-        Person("Anna", "Sidorova", 10),
-        Person("Petr", "Petrov", 11)
-    )
+private fun studentsList(): ArrayList<Person> = arrayListOf(
+    Person("Sveta", "Ivanova", 12),
+    Person("Ivan", "Petrov", 14),
+    Person("Anna", "Sidorova", 10),
+    Person("Petr", "Petrov", 11)
+)
 
-    val list = studentsList().forEach {
-
+private fun sortStudents(list: ArrayList<Person>): ArrayList<Person> {
+    for (i in 1 until list.size) {
+        when (Person.SurnameComparator().compare(list[i - 1].surname, list[i].surname)) {
+            1 -> swap(list, i - 1, i)
+            0 -> {
+                Person.NameComparator().compare(list[i - 1].name, list[i].name)
+                swap(list, i - 1, i)
+            }
+        }
     }
+    return list
+}
 
+private fun swap(list: ArrayList<Person>, previous: Int, current: Int) {
+    val container = list[previous]
+    list[previous] = list[current]
+    list[current] = container
+}
+
+private fun printPersons(list: ArrayList<Person>) {
+    for (i in 0 until list.size) {
+        printPerson(list, i)
+    }
+    println("")
+}
+
+private fun printPerson(list: ArrayList<Person>, index: Int) {
+    val forPrint = list[index]
+    println(forPrint.name + " " + forPrint.surname + " " + forPrint.age)
 
 }
+
+
--- app/src/main/java/com/example/lessonsFromSamsung/theme39_SortingAlgorithms/Person.kt
+++ app/src/main/java/com/example/lessonsFromSamsung/theme39_SortingAlgorithms/Person.kt
@@ -1,19 +1,30 @@
 package com.example.lessonsFromSamsung.theme39_SortingAlgorithms
 
 class Person(
-    name: String,
-    surname: String,
-    age: Int
+    val name: String,
+    val surname: String,
+    val age: Int
 ) {
     class NameComparator : Comparator<String> {
 
-        override fun compare(o1: String, o2: String): Int {
+        override fun compare(n1: String, n2: String): Int {
             return when {
-                o1 == o2 -> 0
-                o1 > o2 -> 1
+                n1 == n2 -> 0
+                n1 > n2 -> 1
                 else -> -1
             }
         }
 
     }
+
+    class SurnameComparator : Comparator<String> {
+        override fun compare(s1: String, s2: String): Int {
+            return when {
+                s1 == s2 -> 0
+                s1 > s2 -> 1
+                else -> -1
+            }
+        }
+    }
+
 }
